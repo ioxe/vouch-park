773b67257eaa3b8d1f20068ff8a11f0c2f2057fa
rashmi-iox <rashmi@ioxengineering.com>
2020-05-12 06:14:34 -0700

    Updated ParkingSignCorrectionService with new ParkingTokenizationService

--- ParkingSignCorrectionService.py
+++ ParkingSignCorrectionService.py
@@ -1,6 +1,7 @@
 import SequenceAlignmentService
 import InputTransformerService
 import ParkingLineCorrectionService
+import ParkingTokenizationService
 
 
 class ParkingSignCorrectionService:
@@ -9,6 +10,7 @@ class ParkingSignCorrectionService:
         self.sequence_alignment_service = SequenceAlignmentService.SequenceAlignmentService()
         self.input_transformer_Service = InputTransformerService.InputTransformerService()
         self.parking_line_correction_service = ParkingLineCorrectionService.ParkingLineCorrectionService()
+        self.parking_tokenization_service = ParkingTokenizationService.ParkingTokenizationService()
 
     def correct_parking_sign(self, inputs):
         merged_sequence = self.sequence_alignment_service.pairwise_align_and_merge_sequences(inputs[0], inputs[1])
@@ -20,7 +22,9 @@ class ParkingSignCorrectionService:
     def dfs(self, node, prev_lines):
         if len(node.val) > 0:
             node.corrections = self.parking_line_correction_service.get_line_corrections(node.val)
+            node.tokens = self.parking_tokenization_service.get_matching_tokens([node.val, node.corrections])
             print(str(node.val) + " : " + str(node.corrections))
+            print("\t\t" + str(node.tokens))
             prev_lines += ", " + node.val
         if len(node.children) == 0:
             return [prev_lines.strip(' ,')]
