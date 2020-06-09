2f773acc2aeae333879f41b5d77334a79aef8b2e
rashmi-iox <rashmi@ioxengineering.com>
2020-06-09 08:02:21 -0700

    Deleting unused files

--- BoundingBox.py
+++
@@ -1,8 +0,0 @@
-class BoundingBox:
-    def __init__(self, origin_x, origin_y, width, height, text, is_important):
-        self.origin_x = origin_x
-        self.origin_y = origin_y
-        self.width = width
-        self.height = height
-        self.text = text
-        self.is_important = is_important
